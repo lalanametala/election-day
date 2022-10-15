c033779fb2ae5f026da160e9006106de8f914428
Lalá Nametala <lalanametala@gmail.com>
2022-10-15 00:33:04 -0300

    :test_tube: test(TestThirdReq): implements the PrintResult tests

--- src/election-day.Test/TestThirdReq.cs
+++ src/election-day.Test/TestThirdReq.cs
@@ -18,6 +18,38 @@ public class TestThirdReq
         int expectedReceivedOption3,
         int expectedOptionNull)
     {
-        throw new NotImplementedException();
+        BallotBox instance = new();
+
+        string stringReaderEntry = "";
+
+        foreach (var entry in entrys)
+        {
+            stringReaderEntry += entry + "\n";
+        }
+        
+        string[] response;
+        using (var stringWriter = new StringWriter())
+        {
+            Console.SetOut(stringWriter);
+            using (var stringReader = new StringReader(stringReaderEntry))
+            {
+                Console.SetIn(stringReader);
+                instance.Start(entrys.Length);
+            }
+        }
+        using (var stringWriter = new StringWriter())
+        {
+            Console.SetOut(stringWriter);
+            instance.PrintResult();
+            response = stringWriter.ToString().Trim().Split('\n');
+        }
+        int[] allVotes = {expectedReceivedOption1, expectedReceivedOption2, expectedReceivedOption3, expectedOptionNull};
+        for (int index = 0; index < response.Length; index++)
+        {
+            if(index < allVotes.Length - 1) response[index].Should().Be("A opção" +(index + 1)+" recebeu: "+allVotes[index]+" voto(s)");
+            else response[index].Should().Be("Total de votos anulados: "+allVotes[index]+" voto(s)");
+        }
+
+
     }
 }
